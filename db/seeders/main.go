a0b58217ed1ff852e428610c6d136fbb609851a8
2072008NicholasCH <nicholas.hanafi2@gmail.com>
2023-03-23 13:02:21 +0700

    initial commit

--- db/seeders/main.go
+++ db/seeders/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"context"
+	"gin-starter-gits/config"
+	"gin-starter-gits/entity"
+	"gin-starter-gits/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 func main() {
 
+	cfg, err := config.LoadConfig(".env")
+	checkError(err)
+
+	db, err := utils.NewPostgresGormDB(&cfg.Postgres)
+	checkError(err)
+
+	if cfg.Env == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	createrSampleUser(db)
+}
+
+func createrSampleUser(db *gorm.DB) {
+	userUUID := uuid.New()
+	if err := db.WithContext(context.Background()).Model(&entity.User{}).Create(entity.NewUser(
+		userUUID,
+		"Nicholas CH",
+		"nicholas.hanafi@digits.id",
+		"test123",
+		"system",
+	)).Error; err != nil {
+		panic(err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ entity/user.entity.go
@@ -0,0 +1,65 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	userTableName = "main.users"
+)
+
+type User struct {
+	ID       int64     `json:"id"`
+	UUID     uuid.UUID `json:"uuid"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
+	Auditable
+}
+
+func (model *User) TableName() string {
+	return userTableName
+}
+
+func NewUser(
+	uuid uuid.UUID,
+	name string,
+	email string,
+	password string,
+	createdBy string,
+) *User {
+	passwordHash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return &User{
+		UUID:      uuid,
+		Name:      name,
+		Email:     email,
+		Password:  string(passwordHash),
+		Auditable: NewAuditable(createdBy),
+	}
+}
+
+func (model *User) MapUpdateFrom(from *User) *map[string]interface{} {
+	if from == nil {
+		return &map[string]interface{}{
+			"name":       model.Name,
+			"email":      model.Email,
+			"updated_at": model.UpdatedAt,
+		}
+	}
+	mapped := make(map[string]interface{})
+
+	if model.Name != from.Name {
+		mapped["name"] = from.Name
+	}
+
+	if model.Email != from.Email {
+		mapped["email"] = from.Email
+	}
+
+	mapped["updated_at"] = time.Now()
+	return &mapped
+}
